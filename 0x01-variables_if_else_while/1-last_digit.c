90237e45e29079380402eabb3c09bda6fc80c8c2
naxus1 <naxus1@gmail.com>
2019-02-07 19:19:21 +0000

    deleted the first printf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,17 +17,17 @@ result = n % 10;
 
 if (result > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, result);
+printf("Last digit of %d is %d and is greater than 5\n", n, result);
 }
 
 else if (result == 0)
 {
-printf("Last digit of %d is 0 and is 0", n);
+printf("Last digit of %d is 0 and is 0\n", n);
 }
 
-else
+else if (result < 6 &&  n != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, result);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, result);
 }
 return (0);
 }
