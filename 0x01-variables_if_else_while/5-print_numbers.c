a0f61e304e1d91ef28cc1c3e30b2b0209b235f5a
naxus1 <naxus1@gmail.com>
2019-02-07 23:07:33 +0000

    Point 5 creted - Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program retuns the numbers in base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num;
+
+for (num = 0; num < 10; num++)
+{
+printf("%d", num);
+}
+
+printf("\n");
+return (0);
+}
