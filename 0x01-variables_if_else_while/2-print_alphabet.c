b61868ec060e8d403491ba8a0d5c1045d4d71fa4
naxus1 <naxus1@gmail.com>
2019-02-07 22:18:17 +0000

    Point 2 creted

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* This function return the alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alph = 'a';
+char new_line = '\n';
+
+while (alph <= 'z')
+{
+putchar(alph);
+c++;
+}
+putchar(new_line);
+return (0);
+}
