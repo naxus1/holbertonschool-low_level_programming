b8ec9ae958c54c02e5a881f9b91423e5e781d8fe
naxus1 <naxus1@gmail.com>
2019-02-07 18:54:45 +0000

    deleted the first printf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,21 +15,19 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 result = n % 10;
 
-printf("Last digit of %d is %d and", n, result);
-
 if (result > 5)
 {
-printf(" is greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5", n, last_number);
 }
 
 else if (result == 0)
 {
-printf(" is 0\n");
+printf("Last digit of %d is 0 and is 0", n);
 }
 
 else
 {
-printf(" is less than 6 and not 0");
+printf("Last digit of %d is %d and is less than 6 and not 0", n, last_number);
 }
 return (0);
 }
