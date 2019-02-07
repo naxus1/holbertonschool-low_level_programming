ce712fc2d3bd99e063d680d737e5ee7b1a5f7aea
naxus1 <naxus1@gmail.com>
2019-02-07 22:46:04 +0000

    The point 4 is created

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * This program print the alphabet in lowercase and uppercase
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+char alp_low;
+char alp_upp;
+char new_line;
+
+alp_low = 'a';
+alp_upp = 'A';
+new_line = '\n';
+
+while (alp_low <= 'z')
+{
+putchar(alp_low);
+alp_low++;
+}
+
+while (alp_upp <= 'Z')
+{
+putchar(alp_upp);
+alp_upp++;
+}
+
+putchar(new_line);
+}
