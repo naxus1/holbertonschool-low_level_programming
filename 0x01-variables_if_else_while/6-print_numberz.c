782b5020f6579bf018c7941407d7953b4d92c399
naxus1 <naxus1@gmail.com>
2019-02-08 00:20:33 +0000

    The point 6 created, Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *the program return the alphabet using putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num;
+int new_line;
+
+new_line = '\n';
+
+for (num = '1'; num <= '9'; num++)
+{
+putchar(num);
+}
+
+putchar(new_line);
+
+return (0);
+}
