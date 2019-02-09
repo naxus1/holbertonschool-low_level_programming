85303632dc143ec2aa88165219dc5371b9e7651f
naxus1 <naxus1@gmail.com>
2019-02-09 17:25:08 +0000

    it point 11 is created

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * this program prints all posible diferent combination of two digits
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-
 	int num;
 	int num1;
 
 	for (num = '0'; num <= '9'; num++)
 	{
-		for (num1 = '0'; num1 <= '9'; num1++)
+		for (num1 = num + 1; num1 <= '9'; num1++)
 		{
 			putchar(num);
 			putchar(num1);
-			if (num < '9')
+
+			if(num != '8' || num1 != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
+
+
 		}
+
 	}
 
-	return (0);
+	putchar('\n');
+	return(0);
 }
