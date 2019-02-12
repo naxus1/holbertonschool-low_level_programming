eeb943b14c006fbddfc2a3b2210c37cc89796bee
naxus1 <naxus1@gmail.com>
2019-02-12 20:50:04 +0000

    it created the point 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,27 @@
 #include "holberton.h"
-
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 void jack_bauer(void)
 {
-	int h1, h2, s1, s2;
+	int h1, h2, m1, m2;
 
-	for (h1 = '0'; h1 <= '9'; h1++)
+	for (h1 = '0'; h1 <= '2'; h1++)
 	{
-		for (h2 = '0'; h2 <= '9'; h2++)
+		for (h2 = '0'; h2 <= '3'; h2++)
 		{
-			for (m1 = '0'; m1 <= '9'; m1++)
+			for (m1 = '0'; m1 <= '5'; m1++)
 			{
 				for (m2 = '0'; m2 <= '9'; m2++)
 				{
 					_putchar(h1);
 					_putchar(h2);
+					_putchar(':');
 					_putchar(m1);
 					_putchar(m2);
+					_putchar('\n');
 				}
 			}
 		}
