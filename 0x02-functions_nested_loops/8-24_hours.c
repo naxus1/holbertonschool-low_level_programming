8799f81ad77a65b6e56b73ba5f6de8e27829604f
naxus1 <naxus1@gmail.com>
2019-02-12 21:46:09 +0000

    it created point 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,24 +6,18 @@
  */
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+	int h1, h2;
 
-	for (h1 = '0'; h1 <= '2'; h1++)
+	for (h1 = 0; h1 <= 23; h1++)
 	{
-		for (h2 = '0'; h2 <= '3'; h2++)
+		for (h2 = 0; h2 <= 59; h2++)
 		{
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-				for (m2 = '0'; m2 <= '9'; m2++)
-				{
-					_putchar(h1);
-					_putchar(h2);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m2);
-					_putchar('\n');
-				}
-			}
+			_putchar(h1 / 10 + '0');
+			_putchar(h1 % 10 + '0');
+			_putchar(':');
+			_putchar(h2 / 10 + '0');
+			_putchar(h2 % 10 + '0');
+			_putchar('\n');
 		}
 	}
 }
