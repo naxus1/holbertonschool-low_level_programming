21259cdeaa00a594fcf30cf567805c0cfbff6ecb
naxus1 <naxus1@gmail.com>
2019-02-13 17:00:34 +0000

    Sintax errors are corrected

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,17 @@
 #include "holberton.h"
 
+/**
+ * add - sum two numbers
+ * @n1:number one to process
+ * @n2:number two to processp
+ * Return: Always 0 (Success)
+ */
+
 int add(int n1, int n2)
 {
 	int result;
 
 	result = n1 + n2;
 
-	return result;
+	return (result);
 }
