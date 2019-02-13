5910f9e18dcb90f60f30669688905ec19cf530ef
naxus1 <naxus1@gmail.com>
2019-02-13 21:38:12 +0000

    Point 13 is created

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int number, sum;
+
+	sum = 0;
+
+	for(number = 0; number < 1024; number++)
+	{
+		if(number % 3 == 0 || number % 5 == 0)
+		{
+			sum = sum + number;
+		}
+	}
+
+	printf ("%d", sum);
+
+	return (0);
+}
