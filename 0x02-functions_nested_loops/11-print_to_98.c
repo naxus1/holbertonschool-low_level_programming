6e3ce91c5a40bab3f627b46a0e50a197770d2fc6
naxus1 <naxus1@gmail.com>
2019-02-13 21:06:22 +0000

    Update point 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,104 +1,44 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
  * print_to_98 - Entry point
  *
  * Return: Always 0 (Success)
  */
-void quotes_spaces(void);
-void print_result(int i);
 
 void print_to_98(int n)
 {
-	int iterator, mod, neg_num;
+	int iterator;
 
 	if (n < 99)
 	{
 		for (iterator = n; iterator <= 98; iterator++)
 		{
-			if (iterator / 10 == 0)
+			printf("%d", iterator);
+
+			if(iterator != 98)
 			{
-				if (iterator < 0)
-				{
-					_putchar('-');
-					neg_num = iterator * -1;
-					_putchar(neg_num + '0');
-					quotes_spaces();
-				}
-
-				else
-				{
-					_putchar(iterator + '0');
-					quotes_spaces();
-				}
-			}
-			else
-			{
-
-				if (iterator < 0)
-				{
-					_putchar('-');
-					neg_num = iterator * -1;
-					print_result(iterator);
-				}
-
-				else
-				{
-					print_result(iterator);
-				}
-
-				if (iterator < 98)
-				{
-					quotes_spaces();
-				}
+				printf(",");
+				printf(" ");
 			}
 		}
 	}
 
 	else
 	{
+		for (iterator = n; iterator >= 98; iterator--)
+		{
+			printf("%d", iterator);
 
-			for (iterator = n; iterator >= 98; iterator--)
+			if (iterator != 98)
 			{
-
-				if (iterator <= 99)
-				{
-					print_result(iterator);
-					if (iterator != 98)
-					{
-						quotes_spaces();
-					}
-				}
-				else
-				{
-					mod = iterator / 10;
-					print_result(iterator);
-					_putchar(iterator % 10 + '0');
-					quotes_spaces();
-				}
+				printf(",");
+				printf(" ");
 			}
+		}
+
+		
 	}
 
-	_putchar('\n');
-}
-/**
- * quotes_spaces - Entry point
- *
- * Return: Always 0 (Success)
- */
-void quotes_spaces(void)
-{
-	_putchar(',');
-	_putchar(' ');
-}
-
-/**
- * print_result - Entry point
- * @i: character
- * Return: Always 0 (Success)
- */
-
-void print_result(int i)
-{
-	_putchar(i / 10 + '0');
-	_putchar(i % 10 + '0');
+	printf("\n");
 }
