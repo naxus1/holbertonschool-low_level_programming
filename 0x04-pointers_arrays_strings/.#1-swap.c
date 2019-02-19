03b70603f126f6f7522fcc10b9c9aa7e4f370a1f
naxus1 <naxus1@gmail.com>
2019-02-19 12:14:53 +0000

    Create point 0 an 1

---
+++ 0x04-pointers_arrays_strings/.#1-swap.c
@@ -0,0 +1,1 @@
+root@vagrant-ubuntu-trusty-64.3829:1550499853
---
+++ 0x04-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - Entry point
+ *@n: memory dirección
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
