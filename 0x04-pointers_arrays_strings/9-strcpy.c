faab9d9cacaf0b3fc8f743039734baa5fd0f6720
naxus1 <naxus1@gmail.com>
2019-02-20 00:25:03 +0000

    it created coments and code for search size of array

--- 0x04-pointers_arrays_strings/9-strcpy.c
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 #include "holberton.h"
-
+/**
+ * *_strcpy - Entry point
+ *@dest: array
+ *@src: string array
+ * Return: Always 0 (Success)
+ */
 char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i, crash, contador;
 
-	for (i = 0; i <= 98; i++)
+	crash = 1;
+	contador = 0;
+
+	while (crash)
+	{
+		if (*(src + contador) != '\0')
+		{
+			contador++;
+		}
+		else
+			crash = 0;
+	}
+
+
+	for (i = 0; i <= contador; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
