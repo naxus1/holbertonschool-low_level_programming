a11e5fda5cc36c7aeb0e0c3ba24c1a57837e098b
naxus1 <naxus1@gmail.com>
2019-02-25 18:40:17 +0000

    Create point 2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strchr - Entry point
+ *@s: String to comparate
+ *@c: Character to search
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	char *stringstr;
+
+	stringstr = s;
+
+	while (*stringstr != '\0')
+	{
+		if (*stringstr == c)
+		{
+			return (stringstr);
+		}
+		stringstr++;
+	}
+
+	return (NULL);
+}
