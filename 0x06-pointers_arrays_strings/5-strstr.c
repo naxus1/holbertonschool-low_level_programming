613b2633e304b32b446d61f4600188f304f1e413
naxus1 <naxus1@gmail.com>
2019-02-26 19:28:03 +0000

    its added conditional for comparate if the string to comparate is empty

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -7,27 +7,28 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, cont, crash;
+	int i, j, sizeStr, crash;
 
-	cont = 0;
 	j = 0;
 	crash = 0;
 
+	for (sizeStr = 0; needle[sizeStr] != '\0'; sizeStr++)
+	{}
 	for (i = 0; (haystack[i] != '\0' && crash == 0); i++)
 	{
+		if (sizeStr == 0)
+		{
+			return (haystack);
+		}
 		if ((haystack[i] == needle[j]) && (needle[j] != '\0'))
 		{
-			cont++;
 			j++;
 		}
-
 		if (needle[j] == '\0')
 		{
 			return (haystack + (i - j + 1));
 			crash = 1;
 		}
-
 	}
-
 	return (0);
 }
