be23c69cb6be332e2c46fe5d04e1ebc3239f0948
naxus1 <naxus1@gmail.com>
2019-03-02 22:12:29 +0000

    The program 0 is review for betty

--- 0x08-static_libraries/holberton.h
+++ 0x08-static_libraries/holberton.h
@@ -1,4 +1,7 @@
-int _putchar(char c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
 int _islower(int c);
 int _isalpha(int c);
 int _abs(int n);
@@ -18,3 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
+#endif
