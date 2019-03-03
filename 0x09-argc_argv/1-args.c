424c1c3d6ad57a57196ea9730572ff743526bb17
naxus1 <naxus1@gmail.com>
2019-03-03 00:11:17 +0000

    Create point 1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @argc: Count of arguments to input
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
