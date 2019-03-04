25d07aea5d1f7a8b9e4fee6a9bb642be681b1fdc
naxus1 <naxus1@gmail.com>
2019-03-04 21:29:10 +0000

    Modified point 100

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -6,50 +6,44 @@
  * @argv: pointer of arguments of input
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int strc, char *strv[])
 {
-	int sum, i, n, cont, flag, flag2;
+	int sum = 0, i, n;
+	int cont = 0, flag, flag2;
 	int arr[5] = {25, 10, 5, 2, 1};
 
-	sum = 0;
-	cont = 0;
-
-	if (argc == 1)
-	{
+	if (strc == 1)
 		printf("Error\n");
-		return (1);
-	}
 	else
 	{
-		n = atoi(argv[1]);
-
-		if(n > 0)
+		n = atoi(strv[1]);
+		if (n < 0)
+			printf("0\n");
+		else
 		{
 			for (i = 0; i < 5; i++)
 			{
 				flag = 0;
-
 				if (arr[i] <= n && flag == 0)
 				{
 					flag2 = 0;
-
 					while (sum <= n && flag2 == 0)
 					{
-						if ((sum + arr[i]) <= n)
+						if (sum + arr[i] <= n)
 						{
 							sum += arr[i];
 							cont++;
 						}
 						else
+						{
 							flag2 = 1;
+						}
 					}
 					flag = 1;
 				}
 			}
-			printf("%d\n", cont);
+			printf("%d", cont);
 		}
-		else
-			printf("%d\n", 0);
 	}
 	return (0);
 }
