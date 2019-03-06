3b90ef175b6130898cb112e9f36e8b5f8c54dda9
naxus1 <naxus1@gmail.com>
2019-03-06 02:55:30 +0000

    Modified my array, removed char

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 
-	myarray = (char*) malloc(sizeof(c) * size);
+	myarray = malloc(sizeof(c) * size);
 
 	if (myarray == NULL)
 	{
