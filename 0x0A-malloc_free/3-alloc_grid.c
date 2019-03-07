5cddf2fe02f8ea417df1d345315472a0f5146964
naxus1 <naxus1@gmail.com>
2019-03-07 15:26:25 +0000

    free memory

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -12,30 +12,31 @@ int **alloc_grid(int width, int height)
 	int **matriz;
 	int i, j;
 
-	if(width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
 
-	matriz = (int **) malloc (height * sizeof(int *));
+	matriz = (int **) malloc(height * sizeof(int *));
 
-	if(matriz == NULL)
+	if (matriz == NULL)
 	{
 		return (0);
 	}
 
-	for(i = 0; i < width; i++)
+	for (i = 0; i < width; i++)
 	{
 		matriz[i] = (int *) malloc(width * sizeof(int));
 
-		if(matriz[i] == NULL)
+		if (matriz[i] == NULL)
 		{
 			return (0);
+			free(matriz);
 		}
 	}
-	for(i = 0; i < height; i++)
+	for (i = 0; i < height; i++)
 	{
-		for(j = 0; j < width; j++)
+		for (j = 0; j < width; j++)
 		{
 			matriz[i][j] = 0;
 		}
