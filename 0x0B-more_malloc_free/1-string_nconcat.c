164bce3e06b143c9d31d96568367d96074db0d3a
naxus1 <naxus1@gmail.com>
2019-03-07 21:06:42 +0000

    Its fix betty Style

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *new_array;
 	unsigned int j, i, ite;
 
-	if(s1 == NULL)
+	if (s1 == NULL)
 		s1 = "";
-	if(s2 == NULL)
+	if (s2 == NULL)
 		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{}
