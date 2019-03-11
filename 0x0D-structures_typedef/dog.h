eea9173f611f8dc40119ffad666967c7de3075a0
naxus1 <naxus1@gmail.com>
2019-03-11 18:28:10 +0000

    Create point 0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+# ifndef  _DOG_H_
+# define  _DOG_H_
+
+/**
+ * struct dog - Dog Data
+ * @name: first member name
+ * @age: second member age
+ * @owner: third member owner
+ */
+
+typedef struct dog
+ {
+	 char *name;
+	 double age;
+	 char *owner;
+ } my_dog;
+
+#endif
