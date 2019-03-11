b61d68afd1397d1dc49ba03aef5baf6a4aa1253a
naxus1 <naxus1@gmail.com>
2019-03-11 18:56:32 +0000

    Create point 1

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - Entry point
+ * @d:Pointer to struct
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ * Return: Always 0 (Success)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	d->age = age;
+	(*d).owner = owner;
+}
--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -14,5 +14,6 @@ typedef struct dog
 	 double age;
 	 char *owner;
 } my_dog;
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
