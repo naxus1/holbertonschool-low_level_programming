2a57f55c4dd064e001b57b39841defeada8d937b
naxus1 <naxus1@gmail.com>
2019-03-11 22:27:29 +0000

    Crete point 2

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef __BASE_FILE__
+#define __BASE_FILE__
+#endif
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __BASE_FILE__);
+	return (0);
+}
