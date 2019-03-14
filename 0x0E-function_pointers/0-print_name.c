fc39d325e2317c311c9bd47bb7dce905c56c98f6
naxus1 <naxus1@gmail.com>
2019-03-14 00:34:27 +0000

    Create point 0

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ * @name:name to print
+ * @f: Function pointer that print.
+ * Return: Always 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0E-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef _FUNTION_POINTER_H_
+#define _FUNTION_POINTER_H_
+
+void print_name(char *name, void (*f)(char *));
+#endif
