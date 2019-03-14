3eb722e762809acd59a4dcd05dfcc01d9ae4e8b0
naxus1 <naxus1@gmail.com>
2019-03-14 02:21:21 +0000

    Its modified type i for size_t and it added library sdlib

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNTION_POINTER_H_
 #define _FUNTION_POINTER_H_
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
