8cd3c9d701abeaa401814b1c3ce0ee2e6bb46384
naxus1 <naxus1@gmail.com>
2019-03-15 19:31:11 +0000

    create point 0

---
+++ 0x0F-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - Add de arguments
+ * @n:Size of argments
+ *
+ * Return: Add the arguments 0 (Success)
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list lista;
+	unsigned int suma = 0, i;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+
+	va_start(lista, n);
+
+	for (i = 0 ; i < n; i++)
+	{
+		suma += (va_arg(lista, int));
+	}
+	return (suma);
+}
