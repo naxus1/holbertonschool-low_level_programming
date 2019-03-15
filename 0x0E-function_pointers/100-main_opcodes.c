8906caad025857cd242d765af167093ab6caa874
naxus1 <naxus1@gmail.com>
2019-03-15 14:05:39 +0000

    point 4  modified

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -8,17 +8,18 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+	register int i, num;
+	char *ptr = (char *)main;
+
 	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-	if (atoi(argv[1] < 0))
-	{
-		printf("Error\n");
-		exit(2);
-	}
+		printf("Error\n"), exit(1);
+	num = atoi(argv[1]);
+	if (num < 0)
+		printf("Error\n"), exit(2);
+	for (i = 0; i < num - 1; i++)
+		printf("%02hhx ", ptr[i]);
+	printf("%02hhx\n", ptr[i]);
 	return (0);
 }
