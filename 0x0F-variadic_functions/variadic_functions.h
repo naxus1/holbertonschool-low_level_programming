d0c44776b09cbded40a5b13c24f39d71c7273c30
naxus1 <naxus1@gmail.com>
2019-03-16 17:47:03 +0000

    [fixed]: its add description to struct

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,5 +1,6 @@
 #ifndef _VARIADIC_H
 #define _VARIADIC_H
+
 #include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
@@ -11,6 +12,13 @@ void print_float(va_list d);
 void print_array(va_list d);
 void print_all(const char * const format, ...);
 
+/**
+ * struct print_function - Struct to print
+ * @my_character: Character to review
+ * @my_print: Function to print
+ *
+ * Description: Struct that print, according to type mycharacter
+ */
 typedef struct print_function
 {
 	char my_character;
