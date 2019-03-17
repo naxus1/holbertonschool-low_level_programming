409429f3d07b713592051558fa921f774914043b
naxus1 <naxus1@gmail.com>
2019-03-17 23:07:20 +0000

    [fixe]:Its added separator en line 26

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		for (i = 0; i < n; i++)
 		{
-			if (i > 0)
+			if (i > 0 && separator != NULL)
 			{
 				printf("%s%d", char_tmp, va_arg(lista, int));
 			}
