4299c4ba1610929fd194c63e134b38579b86f82d
naxus1 <naxus1@gmail.com>
2019-03-17 22:29:55 +0000

    [fixe]:Its modified the beginnig of va_star, its added else

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -12,20 +12,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list lista;
 
-	va_start(lista, n);
-
 	if (separator == NULL)
 		return;
 
-	for (i = 0; i < n; i++)
+	else
 	{
-		if (i > 0)
-		{
-			printf("%s %d", separator, va_arg(lista, int));
-		}
+		va_start(lista, n);
 
-		else
-			printf("%d", va_arg(lista, int));
+		for (i = 0; i < n; i++)
+		{
+			if (i > 0)
+			{
+				printf("%s %d", separator, va_arg(lista, int));
+			}
+
+			else
+				printf("%d", va_arg(lista, int));
+		}
 	}
 
 	va_end(lista);
