8b8d1640347d15c61e814762d4bcc334da506c34
naxus1 <naxus1@gmail.com>
2019-03-18 02:26:24 +0000

    Modified point 1

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -8,19 +8,19 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	int i;
+	unsigned int i;
 	va_list lista;
 	va_start(lista, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (i > 0)
-		{
-			printf("%s %d", separator, va_arg(lista, int));
-		}
+		printf("%d", va_arg(lista, int));
 
-		else
-			printf("%d", va_arg(lista, int));
+		if(i != n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
 	}
+	printf("\n");
 	va_end(lista);
 }
