a3e2bfb8a2dd0bce6a748289a6dff6722c4ea73e
naxus1 <naxus1@gmail.com>
2019-03-18 02:35:08 +0000

    Modified betty style comments

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -1,27 +1,23 @@
 #include "variadic_functions.h"
 #include <stdio.h>
- /**
- * print_numbers - print numbers
- * @separator: separator of string
- * @n: number of  argumentst
+/**
+ * print_numbers - print numbers that are taken in as arguments
+ * @separator: used to differentate one argument from the other
+ * @n: number of arguments taking in that cant be changed
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list lista;
+	va_list list;
 
-	va_start(lista, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(lista, int));
-
+		printf("%d", va_arg(list, int));
 		if (i != n - 1 && separator)
-		{
 			printf("%s", separator);
-		}
 	}
 	printf("\n");
-	va_end(lista);
+	va_end(list);
 }
