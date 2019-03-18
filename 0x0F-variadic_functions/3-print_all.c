8df537d257d4ce1717757608f3391f97db339c81
naxus1 <naxus1@gmail.com>
2019-03-18 01:48:12 +0000

    Modified point 3

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -1,87 +1,46 @@
 #include "variadic_functions.h"
 /**
- * myp_char - Print a character
- * @lista:char to print
- */
-void myp_char(va_list lista)
-{
-	printf("%c", va_arg(lista, int));
-}
-
-/**
- * print_int - Print integer
- * @lista: integer to print
- */
-void print_int(va_list lista)
-{
-	printf("%d", va_arg(lista, int));
-}
-
-/**
- * print_float - Print float
- * @lista: Float to print
- */
-void print_float(va_list lista)
-{
-	printf("%f", (va_arg(lista, double)));
-}
-
-/**
- * print_array - Print strig
- * @lista: string to print
- */
-void print_array(va_list lista)
-{
-	char *p;
-
-	p = va_arg(lista, char *);
-
-	if (p == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	else
-		printf("%s", p);
-}
-
-
-/**
  * print_all - Entry point
  * @format:is a list of types of arguments passed to the function
  *
  */
-
 void print_all(const char * const format, ...)
 {
-	int i = 0, j;
-	char *p;
 	va_list lista;
-	myprint_f my_array[] = {
-		{'c', myp_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_array},
-	};
+	char *p, *word;
 
-	va_start(lista, format);
 	p = "";
+	int i = 0;
 
+	va_start(list, format);
 	while (format[i] != '\0')
 	{
-		j = 0;
-		while (j < 4)
+		switch (format[i])
 		{
-			if (my_array[j].my_character == format[i])
+		case 'c':
+			printf("%s%c", p, va_arg(lista, int));
+			break;
+		case 'i':
+			printf("%s%d", p, va_arg(lista, int));
+			break;
+		case 'f':
+			printf("%s%f", p, va_arg(lista, double));
+			break;
+		case 's':
+			word = va_arg(lista, char *);
+			if (str == NULL)
 			{
-				printf("%s", p);
-				my_array[j].my_print(lista);
-				p = ", ";
+				str = "(nil)";
 			}
-			j++;
+			printf("%s%s", p, word);
+			break;
+		default:
+			i++;
+			continue;
 		}
+		p = ", ";
 		i++;
 	}
 	printf("\n");
-	va_end(lista);
+	va_end(list);
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -8,24 +8,5 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void myp_char(va_list c);
-void print_int(va_list d);
-void print_float(va_list d);
-void print_array(va_list d);
-void print_all(const char * const format, ...);
-
-/**
- * struct print_function - Struct to print
- * @my_character: Character to review
- * @my_print: Function to print
- *
- * Description: Struct that print, according to type mycharacter
- */
-typedef struct print_function
-{
-	char my_character;
-	void (*my_print)(va_list);
-} myprint_f;
-
 
 #endif
