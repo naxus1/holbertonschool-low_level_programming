407e59ebcbd2f72d78db1eafd18078b8a6968ee6
naxus1 <naxus1@gmail.com>
2019-03-21 22:43:39 +0000

    Create point 0

---
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * print_list - Pring to list
+ * @h: header to print
+ * Return: number of elements of list.
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *aux;
+	size_t cont;
+
+	aux = h;
+	cont = 0;
+
+	if(aux != NULL)
+	{
+		while (aux != NULL)
+		{
+			if (aux->str == NULL)
+			{
+				printf("[0]");
+				printf("(nil)\n");
+				cont++;
+			}
+			else
+			{
+				printf("[%d] %s\n", aux->len, aux->str);
+				cont++;
+			}
+			aux = aux->next;
+		}
+	}
+	return (cont);
+
+}
---
+++ 0x11-singly_linked_lists/README.md
@@ -0,0 +1,10 @@
+0x11. C - Singly linked lists
+
+Linked Lists
+Google
+Youtube
+Data Structures
+
+General
+When and why using linked lists vs arrays
+How to build and use linked lists
