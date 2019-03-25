02309cacb87479c4544d205aa6c080bc90461728
naxus1 <naxus1@gmail.com>
2019-03-25 01:14:09 +0000

    Create point 5

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -27,5 +27,6 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *string);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void fun_before();
 
 #endif
