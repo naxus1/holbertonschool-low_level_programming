c7bb18072f89d7bea6a620ef5151de845e8a19e0
naxus1 <naxus1@gmail.com>
2019-03-27 20:58:26 +0000

    Modified point

--- 0x12-more_singly_linked_lists/100-reverse_listint.c
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -8,11 +8,19 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *ptr1, *ptr2;
 
+	if(*head == NULL)
+		return (0);
+
 	ptr1 = *head;
 	*head = (*head)->next;
 	ptr2 = *head;
 	ptr1->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = ptr1;
+		return (*head);
+	}
 	while ((*head)->next != NULL)
 	{
 		*head = (*head)->next;
