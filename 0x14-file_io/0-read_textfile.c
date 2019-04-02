ed87e37dd7af3bbee033533c79feeea59e217566
naxus1 <naxus1@gmail.com>
2019-04-02 19:27:19 +0000

    Created point 0

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "holberton.h"
+/**
+ * read_textfile - reads a text file
+ * @filename: name file
+ * @letters: how many letters to read and print
+ * Return: 0 if error, otherwise number letters printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileDescOpen, fileDescRead, fileDescWrite;
+	char *bufferRead;
+
+	if (!filename || letters == 0)
+		return (0);
+
+	fileDescOpen = open(filename, O_RDWR);
+
+	if (fileDescOpen < 0)
+	{
+		close(fileDescOpen);
+		return (0);
+	}
+
+	bufferRead = calloc(letters, sizeof(char));
+	if (bufferRead == NULL)
+	{
+		close(fileDescOpen);
+		return (0);
+	}
+	fileDescRead = read(fileDescOpen, bufferRead, letters);
+
+	if (fileDescRead < 0)
+	{
+		close(fileDescOpen);
+		free(bufferRead);
+		return (0);
+	}
+
+	fileDescWrite = write(STDOUT_FILENO, bufferRead, letters);
+
+	if (fileDescWrite <= 0)
+	{
+		free(bufferRead);
+		return (0);
+	}
+
+	free(bufferRead);
+	return (fileDescRead);
+}
---
+++ 0x14-file_io/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_P
+#define _HOLBERTON_P
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
