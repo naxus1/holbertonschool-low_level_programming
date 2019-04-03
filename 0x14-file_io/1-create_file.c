ed730de81ad4377c78582f2db804c4b4ea7da89d
naxus1 <naxus1@gmail.com>
2019-04-03 03:30:24 +0000

    Create point 1

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "holberton.h"
-
+/**
+ * create_file - create a file
+ * @filename: name file
+ * @text_content: content of the file
+ * Return: 1 on success, -1 on failure
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int descFileOpen, descFileWrite, letters = 0;
@@ -7,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	descFileOpen = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	descFileOpen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (descFileOpen == -1)
 	{
 		close(descFileOpen);
@@ -16,13 +21,26 @@ int create_file(const char *filename, char *text_content)
 
 	letters = amountLetters(text_content);
 
-	descFileWrite = write(descFileOpen, text_content, letters);
+	if (text_content)
+	{
+		descFileWrite = write(descFileOpen, text_content, letters);
+		if (descFileWrite == -1)
+		{
+			close(descFileOpen);
+			return (-1);
+		}
+	}
 
-	return (descFileWrite);
+	close(descFileOpen);
+	return (1);
 }
+/**
+ * amountLetters - count the character of an string
+ * @words: string for counter
+ * Return: numbers of letters in the string
+ */
 
-
-int  amountLetters (char *words)
+int amountLetters(char *words)
 {
 	int i;
 
--- 0x14-file_io/1-main.c
+++ 0x14-file_io/1-main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "holberton.h"
-
 /**
  * main - check the code for Holberton School students.
  *
