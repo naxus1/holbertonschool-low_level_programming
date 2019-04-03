6dd8face87263f4239980b335141286b846e757e
naxus1 <naxus1@gmail.com>
2019-04-03 03:46:17 +0000

    Modified returns

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,20 +15,18 @@ int create_file(const char *filename, char *text_content)
 	descFileOpen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (descFileOpen == -1)
 	{
-		close(descFileOpen);
 		return (0);
 	}
 
+	if(text_content != NULL)
+		text_content = "";
+
 	letters = amountLetters(text_content);
 
-	if (text_content)
+	descFileWrite = write(descFileOpen, text_content, letters);
+	if (descFileWrite == -1)
 	{
-		descFileWrite = write(descFileOpen, text_content, letters);
-		if (descFileWrite == -1)
-		{
-			close(descFileOpen);
-			return (-1);
-		}
+		return (-1);
 	}
 
 	close(descFileOpen);
