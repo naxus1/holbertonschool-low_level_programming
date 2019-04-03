73bf8af86cf814c761062d388cdaf36c6a676f32
naxus1 <naxus1@gmail.com>
2019-04-03 04:06:31 +0000

    Modified returns

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -1,33 +1,46 @@
 #include "holberton.h"
-
 /**
- * create_file - creates a file.
- * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
- *
+ * create_file - create a file
+ * @filename: name file
+ * @text_content: content of the file
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd, letters = 0, conf;
+	int descFileOpen, descFileWrite, letters = 0;
 
-/* write */
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	if (fd == -1)
+	descFileOpen = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (descFileOpen == -1)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
-	for (; text_content[letters] != '\0'; letters++)
-		;
-	conf = write(fd, text_content, letters);
-	if (conf == -1)
+
+	letters = amountLetters(text_content);
+
+	descFileWrite = write(descFileOpen, text_content, letters);
+	if (descFileWrite == -1)
 		return (-1);
-	close(fd);
+
+	close(descFileOpen);
 
 	return (1);
+}
+/**
+ * amountLetters - count the character of an string
+ * @words: string for counter
+ * Return: numbers of letters in the string
+ */
+
+int amountLetters(char *words)
+{
+	int i;
+
+	for (i = 0; words[i] != '\0'; i++)
+	{}
+
+	return (i);
 }
