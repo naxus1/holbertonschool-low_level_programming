70fac75393161f31231ceace5018f3beab904555
naxus1 <naxus1@gmail.com>
2019-04-07 17:37:08 +0000

    Created advance

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 0;
+	int b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar(a / 10 % 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 % 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99)
+				putchar('\n');
+			else
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+	}
+	return (0);
+}
