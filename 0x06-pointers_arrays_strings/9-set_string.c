ab424bafa8d85f4103c853cd5acd9ca8d3246813
naxus1 <naxus1@gmail.com>
2019-04-07 19:18:33 +0000

    Created point 9

---
+++ 0x06-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * set_string - function
+ * @s: pointer to string
+ * @to: pointer to string
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
