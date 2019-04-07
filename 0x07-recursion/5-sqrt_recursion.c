bd5f1673835bb62dae61cabe6288350b92c93f7e
naxus1 <naxus1@gmail.com>
2019-04-07 19:36:23 +0000

    Created point 5

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -1,31 +1,34 @@
 #include "holberton.h"
 /**
- * _sqrt_recursion -  square root of a number
- * @n: integer
- * Return: number and n to compare
+ * square - returns natural square root of a number
+ * @s: num to find square root of
+ * @i: num to check against numer
+ * If no natural square root, returns -1
+ * Return: square root of n
+ */
+int square(int s, int i)
+{
+	int x;
+
+	x = i * i;
+	if (x > s)
+		return (-1);
+	if (x == s)
+		return (i);
+	return (square(s, i + 1));
+}
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: num to find square root of
+ *
+ * If no natural square root, returns -1
+ * Return: square root of n
  */
 int _sqrt_recursion(int n)
 {
-	return (prime(1, n));
-}
-/**
- * prime - find square number
- * @x: integer
- * @y: integer
- * Return: return x compared to y
- */
-int prime(int x, int y)
-{
-	if (x * x == y)
-	{
-		return (x);
-	}
-	else if (x * x > y)
-	{
+	if (n < 0)
 		return (-1);
-	}
-	else
-	{
-		return (prime(x += 1, y));
-	}
+	if (n == 0 || n == 1)
+		return (n);
+	return (square(n, 1));
 }
