69c2bc55e0a7027c1937f798cbc857ce4eacba85
naxus1 <naxus1@gmail.com>
2019-05-05 20:20:47 +0000

    create point 7

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (0);
 	}
 	if (h == NULL)
-	{free(newNode);
+	{free(new_nodo);
 		return (NULL);
 	}
 	if (idx == 0)
