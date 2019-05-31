87eefb5884d88079c7724b5e94971403bfe3189c
naxus1 <naxus1@gmail.com>
2019-05-31 15:44:00 +0000

    Create point 0

---
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the hash_table_t
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int num;
+
+	if (size <= 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	num = 0;
+	while (num < size)
+	{
+		table->array[num] = NULL;
+		++num;
+	}
+	return (table);
+}
