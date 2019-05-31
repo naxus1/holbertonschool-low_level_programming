b77ab72c25113cb0d8f71fd81c42b42de4e27c07
naxus1 <naxus1@gmail.com>
2019-05-31 23:22:07 +0000

    Betty

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,45 +1,41 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - creates a hash table
- * @ht: size of the array
- * @key:
- * @value:
- * Return: pointer to the hash_table_t
+ * hash_table_set - Function to add a new elemnt
+ *
+ * @ht: pointer to hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: success
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
 	unsigned long int index;
+	const unsigned char *keynew;
+	hash_node_t *newNode;
 	hash_node_t *aux;
 
-	const unsigned char *new_key = (const unsigned char *) key;
-
-	if (!ht || !strlen(key) || !ht->size)
-		return (0);
-
-	index = key_index(new_key, ht->size);
+	keynew = (const unsigned char *)key;
+	index = key_index(keynew, ht->size);
 	aux = ht->array[index];
 
-	if (aux != NULL)
+	if (ht == NULL || value == NULL || key == NULL)
+		return (0);
+	while (aux != NULL)
 	{
-		while (aux != NULL)
+		if (strcmp(key,aux->key) == 0)
 		{
-			if (strcmp(aux->key, key) == 0)
-			{
-				aux->value = strdup(value);
-				return (1);
-			}
-			aux = aux->next;
+			aux->value = strdup(value);
+			return (1);
 		}
+		aux = aux->next;
 	}
-
-	new_node = malloc(sizeof(hash_node_t));
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
+	newNode = malloc(sizeof(hash_node_t));
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = ht->array[index];
+	ht->array[index] = newNode;
 	return (1);
 }
