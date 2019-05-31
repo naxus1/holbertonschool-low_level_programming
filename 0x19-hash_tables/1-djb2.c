9d16d3428498411480ec26fe22e29e56996c531e
naxus1 <naxus1@gmail.com>
2019-05-31 16:01:43 +0000

    Create point 1

--- 0x19-hash_tables/1-djb2.c
+++ 0x19-hash_tables/1-djb2.c
@@ -4,8 +4,8 @@
  * @str: str to hash
  *
  * Return: hash
- */
-hash(unsigned char *str)
+  */
+unsigned long int hash_djb2(const unsigned char *str)
 {
         unsigned long hash = 5381;
         int c;
