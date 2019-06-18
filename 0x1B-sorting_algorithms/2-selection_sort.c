f02cdb7c7d2f0d3ee6cda5d5123d9647cfc2e470
naxus1 <naxus1@gmail.com>
2019-06-18 15:43:24 +0000

    add conditional

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -12,30 +12,32 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, cont;
 	int index, temp;
 
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		/* Find the smallest integer */
-		index = i;
-		for (j = i + 1; j < size; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] < array[index])
-				index = j;
-		}
-		if (index != (int)i)
-		{
-			/* Swap */
-			temp = array[index];
-			array[index] = array[i];
-			array[i] = temp;
-
-			/* Print */
-			for (cont = 0; cont < size; cont++)
+			/* Find the smallest integer */
+			index = i;
+			for (j = i + 1; j < size; j++)
 			{
-				printf("%d", array[cont]);
-				if (cont < size - 1)
-					printf(" ");
+				if (array[j] < array[index])
+					index = j;
 			}
-			printf("\n");
+			if (index != (int)i)
+			{
+				/* Swap */
+				temp = array[index];
+				array[index] = array[i];
+				array[i] = temp;
+				/* Print */
+				for (cont = 0; cont < size; cont++)
+				{
+					printf("%d", array[cont]);
+					if (cont < size - 1)
+						printf(", ");
+				}
+				printf("\n");
+			}
 		}
 	}
 }
